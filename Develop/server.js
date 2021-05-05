7a1507c47b5d86272e0541bf6a346932ba4859f5
Raul Sanz <luar_znas@hotmail.com>
2021-05-05 10:50:12 +0100

    JS: Setting server.js and the routes

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+const express = require('express');
+
+// Sets an initial port
+const PORT = 3000;
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// LISTENER
+// The below code effectively "starts" our server
+
+app.listen (PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+});
